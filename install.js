4d0808f4836e79c27ee08e197747386e9c2b8793
Mix <32300164+mnixry@users.noreply.github.com>
2022-01-22 15:47:36 +0800

    bugfix: fix incorrect install function parameters
    Fixes 404 not found reported during post installation step

--- install.js
+++ install.js
@@ -1,10 +1,16 @@
 async function install() {
-  const { downloadRelease, getReleaseByTag } = require('./lib/install')
-
-  const release = await getReleaseByTag('v1.0.0-beta8-fix2')
-  await downloadRelease(release)
+  const {
+    downloadRelease,
+    getReleaseByTag,
+    getLatestRelease,
+  } = require("./lib/install");
+  const release =
+    process.env.GOCQHTTP_LATEST_RELEASE === "true"
+      ? await getLatestRelease("Mrs4s/go-cqhttp")
+      : await getReleaseByTag("Mrs4s/go-cqhttp", "v1.0.0-beta8-fix2");
+  await downloadRelease(release);
 }
 
-if (__dirname.includes('node_modules')) {
-  install()
+if (__dirname.includes("node_modules")) {
+  install();
 }
