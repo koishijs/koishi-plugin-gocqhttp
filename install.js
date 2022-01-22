178fd7d6afd77d18d9b34aa18af2515cf275b370
Mix <32300164+mnixry@users.noreply.github.com>
2022-01-22 15:52:10 +0800

    chore: optimize code style

--- install.js
+++ install.js
@@ -3,14 +3,14 @@ async function install() {
     downloadRelease,
     getReleaseByTag,
     getLatestRelease,
-  } = require("./lib/install");
+  } = require('./lib/install')
   const release =
-    process.env.GOCQHTTP_LATEST_RELEASE === "true"
-      ? await getLatestRelease("Mrs4s/go-cqhttp")
-      : await getReleaseByTag("Mrs4s/go-cqhttp", "v1.0.0-beta8-fix2");
+    process.env.GOCQHTTP_LATEST_RELEASE === 'true'
+      ? await getLatestRelease('Mrs4s/go-cqhttp')
+      : await getReleaseByTag('Mrs4s/go-cqhttp', 'v1.0.0-beta8-fix2')
   await downloadRelease(release);
 }
 
-if (__dirname.includes("node_modules")) {
-  install();
+if (__dirname.includes('node_modules')) {
+  install()
 }
