649d7dd5c995c7a89ee3a48e695f9eee1e945bb2
Mix <32300164+mnixry@users.noreply.github.com>
2022-01-22 15:53:43 +0800

    chore: code style

--- install.js
+++ install.js
@@ -8,7 +8,7 @@ async function install() {
     process.env.GOCQHTTP_LATEST_RELEASE === 'true'
       ? await getLatestRelease('Mrs4s/go-cqhttp')
       : await getReleaseByTag('Mrs4s/go-cqhttp', 'v1.0.0-beta8-fix2')
-  await downloadRelease(release);
+  await downloadRelease(release)
 }
 
 if (__dirname.includes('node_modules')) {
