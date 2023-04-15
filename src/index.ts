f014bddfc20ca5ce871b2c853c158c1eac4d5e7b
Shigma <shigma10826@gmail.com>
2023-04-15 23:45:59 +0800

    fix: fix incorrect endpoint for ws and http

--- src/index.ts
+++ src/index.ts
@@ -165,7 +165,7 @@ class Launcher extends DataService<Dict<Data>> {
       ...bot.config.gocqhttp,
     }
     if ('endpoint' in config) {
-      config.endpoint = `0.0.0.0:${new URL(config.endpoint).port}`
+      config.endpoint = `127.0.0.1:${this.ctx.router.port}`
     }
     if ('path' in config) {
       const { port, host = 'localhost' } = bot.ctx.root.config
